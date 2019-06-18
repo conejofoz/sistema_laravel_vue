976b8bab6d3c221af5c3cc89030674af4ed5b9c9
conejofoz <silviocoelho.udc@gmail.com>
2019-06-18 16:39:34 -0300

    migrations e model

---
+++ app/Categoria.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Categoria extends Model
+{
+    //protected $table = 'categorias';
+    /*laravel eloquent já assume que o nome da tabela vai ser o nome da classe com a primeira letra minuscula*/
+    //protected $primaryKey = 'id';
+    /*também não precisa pq eloquent já assuma que a chave primaria se chamará id*/
+
+    protected $fillable = ['nombre', 'descripcion', 'condicion'];
+    /**proteger os campos aos quais vamos enviar valores */
+}
