c8a83845c7c96a203251615d7963cf74cd2f6c6f
conejofoz <silviocoelho.udc@gmail.com>
2019-07-13 13:10:09 -0300

    login de usuario

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
+    'failed' => 'Estas credenciais não coincidem com nossos registros.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
